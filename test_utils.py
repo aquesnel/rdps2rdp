c78077c427c8efcf3e8298ecf367c00b7bfb2111
Alexandre Quesnel <131881+aquesnel@users.noreply.github.com>
2022-05-30 02:17:04 +0000

    Add test_utils.load_snapshot

--- test_utils.py
+++ test_utils.py
@@ -1,5 +1,8 @@
+import os
+import json
 
 from parser_v2 import RdpContext, ChannelDef
+import parser_v2_context
 
 def extract_as_bytes(data):
     result = ''
@@ -11,3 +14,9 @@ def extract_as_bytes(data):
 
 def extract_as_context(values):
     return RdpContext(**values)
+
+SELF_DIR = os.path.dirname(__file__)
+def load_snapshot(file_name):
+    with open(SELF_DIR + '/test_data/' + file_name, 'r') as f:
+        snapshot = parser_v2_context.RdpStreamSnapshot.from_json(json.load(f))
+    return snapshot
